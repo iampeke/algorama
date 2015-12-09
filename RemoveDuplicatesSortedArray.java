d919da28617b9dcdef02080c224c3db3082b304e
Peke! <pekelike@gmail.com>
2015-12-09 11:00:32 -0800

    Create RemoveDuplicatesSortedArray.java

---
+++ RemoveDuplicatesSortedArray.java
@@ -0,0 +1,16 @@
+public class RemoveDuplicatesSortedArray {
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        int j = 1;
+        while (j < nums.length) {
+            if (nums[i] == nums[j]) {
+                j++;
+            } else {
+                i++;
+                nums[i] = nums[j];
+                j++;
+            }
+        }
+        return i+1;
+    }
+}
